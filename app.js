1996cfb2c28dcb9656d8d5b444af67709f910567
manupuranic <manupuranic@gmail.com>
2023-08-16 17:01:10 +0530

    user Table relations

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 // const { mongoConnect } = require("./util/database");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("64db7a1221966eaf2cb578c2")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("64dcb036890263abd0073bc7")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -36,6 +36,17 @@ app.use(errorController.get404);
 mongoose
   .connect(process.env.DB_URL)
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        User.create({
+          name: "Manu",
+          email: "manu@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+      }
+    });
     console.log("connected");
     app.listen(3000, () => {
       console.log("starting the server at 3000");
--- models/product.js
+++ models/product.js
@@ -10,6 +10,11 @@ const productSchema = new Schema({
   price: Number,
   imageUrl: String,
   description: String,
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Product", productSchema);
