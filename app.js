9d02038acdc9cdf56b78b84c728b71e7391dbad8
manupuranic <manupuranic@gmail.com>
2023-08-16 13:15:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const path = require("path");
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const { mongoConnect } = require("./util/database");
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("64db7a1221966eaf2cb578c2")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
